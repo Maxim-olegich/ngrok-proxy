eaa2715e92d0613927b51234b8976c8673886b5e
Maxim-olegich <113010540+Maxim-olegich@users.noreply.github.com>
2025-06-05 14:16:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,15 +5,7 @@ const app = express();
 
 const NGROK_URL = process.env.NGROK_URL || "https://example.ngrok-free.app";
 
-app.use((req, res, next) => {
-  if (!req.headers["ngrok-skip-browser-warning"]) {
-    // Если заголовка нет — возвращаем простой текст,
-    // чтобы избежать включения предупреждения Ngrok
-    return res.send("Ngrok proxy running. Please use client with proper header.");
-  }
-  next();
-});
-
+// Просто проксируем все запросы и добавляем нужный заголовок к исходящему запросу
 app.use("/", createProxyMiddleware({
   target: NGROK_URL,
   changeOrigin: true,
